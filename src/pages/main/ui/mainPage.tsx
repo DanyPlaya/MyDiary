5f29e5cbe22f286c56a9bae27601924e32f071dd
DanyPlaya <danila.lusuy@gmail.com>
2023-11-08 23:24:26 +0300

    feat: add NoteList and add responsive

--- src/pages/main/ui/mainPage.tsx
+++ src/pages/main/ui/mainPage.tsx
@@ -1,3 +1,5 @@
+import { NoteCard } from "@/entities/note";
+import { NoteList } from "@/widgets/noteList";
 import { Box, Text } from "@chakra-ui/react";
 
 export const MainPage = () => {
@@ -5,25 +7,30 @@ export const MainPage = () => {
     <Box
       p={"80px 40px 237px 40px"}
       borderRadius={{
-        lg: "80px 40px 237px 40px",
-        md: "80px 40px 237px 40px",
-        sm: "30px 30px 0px 0px",
+        lg: "80px 80px 0px 0px",
+        md: "80px 80px 0px 0px",
+        sm: "30px 30px 0 0",
+        base: "30px 30px 0 0",
       }}
       bg={"#F6F6F6"}
-      h={"100vh"}
+      h={"100vh - 70px"}
     >
-      <Text
-        fontSize={{ lg: "112px", md: "72px", sm: "42px" }}
-        fontWeight={"700"}
-      >
-        Мой
-      </Text>
-      <Text
-        fontSize={{ lg: "112px", md: "72px", sm: "42px" }}
-        fontWeight={"700"}
-      >
-        дневничок
-      </Text>
+      <Box>
+        <Text
+          fontSize={{ lg: "112px", md: "72px", sm: "42px" }}
+          fontWeight={"700"}
+          lineHeight={"line-height: 90%"}
+        >
+          Мой
+        </Text>
+        <Text
+          fontSize={{ lg: "112px", md: "72px", sm: "42px" }}
+          fontWeight={"700"}
+        >
+          дневничок
+        </Text>
+      </Box>
+      <NoteList />
     </Box>
   );
 };
