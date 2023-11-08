32dd269e7c8dd572c5b0d999fb818b98416a1b83
DanyPlaya <danila.lusuy@gmail.com>
2023-11-08 23:19:44 +0300

    feat: added redux provider

--- src/app/appEntry.tsx
+++ src/app/appEntry.tsx
@@ -3,11 +3,15 @@ import { RouterProvider } from "react-router-dom";
 import { AppRouter } from "./appRouter";
 import customeTheme from "./chakraTheme";
 import "@fontsource-variable/nunito";
-
+import { createStore } from "./appStore";
+import { Provider } from "react-redux";
+const store = createStore();
 export const AppEntry = () => {
   return (
-    <ChakraProvider theme={customeTheme}>
-      <RouterProvider router={AppRouter()} />
-    </ChakraProvider>
+    <Provider store={store}>
+      <ChakraProvider theme={customeTheme}>
+        <RouterProvider router={AppRouter()} />
+      </ChakraProvider>
+    </Provider>
   );
 };
