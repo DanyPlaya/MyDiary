86e2890e277bee2eb6815b5cc41b79a1f6151f3e
DanyPlaya <danila.lusuy@gmail.com>
2023-11-08 14:58:25 +0300

    feat: add adaptive styles for borderRadius

--- src/pages/main/ui/mainPage.tsx
+++ src/pages/main/ui/mainPage.tsx
@@ -4,8 +4,13 @@ export const MainPage = () => {
   return (
     <Box
       p={"80px 40px 237px 40px"}
-      borderRadius={"80px 80px 0px 0px"}
+      borderRadius={{
+        lg: "80px 40px 237px 40px",
+        md: "80px 40px 237px 40px",
+        sm: "30px 30px 0px 0px",
+      }}
       bg={"#F6F6F6"}
+      h={"100vh"}
     >
       <Text
         fontSize={{ lg: "112px", md: "72px", sm: "42px" }}
