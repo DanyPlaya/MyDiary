3d694f541ffd85f89ccb483e1a1035f80b2f6727
DanyPlaya <danila.lusuy@gmail.com>
2023-11-08 14:49:40 +0300

    feat: remove bottom props

--- src/widgets/footer/ui/footer.tsx
+++ src/widgets/footer/ui/footer.tsx
@@ -2,7 +2,7 @@ import { Box, Text } from "@chakra-ui/react";
 
 export const Footer = () => {
   return (
-    <Box bottom="0" p={"25px 40px 25px 40px"} bg={"#050F28"} h={"70px"}>
+    <Box p={"25px 40px 25px 40px"} bg={"#050F28"} h={"70px"}>
       <Text
         color={"white"}
         fontSize={"18px"}
