9c6675e3e5dddfa56913ee16edd3214603ad9b8e
DanyPlaya <danila.lusuy@gmail.com>
2023-11-09 19:54:10 +0300

    fix: please deploy

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import pkg from 'json-server';
 import path from 'path';
-const { bodyParser, create, defaults, router,rewriter } = pkg;
+const { create, defaults, router,rewriter } = pkg;
 
 
 const server = create();
@@ -9,18 +9,19 @@ const server = create();
 // eslint-disable-next-line no-undef
 const myRouter = router(path.resolve('./api/db.json'));
 
-server.use(defaults({}));
-server.use(bodyParser);
+
 
 // Нужно для небольшой задержки, чтобы запрос проходил не мгновенно, имитация реального апи
 
 
+server.use(defaults({}));
 
-server.use(myRouter);
 server.use(rewriter({
     '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
     
 }))
+server.use(myRouter);
 // запуск сервера
 server.listen(3000, () => {
     console.log('server is running on 8000 port');
