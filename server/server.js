6e29be6a73df37b65771f8edb63b176c6237f519
DanyPlaya <danila.lusuy@gmail.com>
2023-11-09 14:47:24 +0300

    feat: create simple server

---
+++ server/server.js
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import pkg from 'json-server';
+import path from 'path';
+const { bodyParser, create, defaults, router } = pkg;
+
+
+const server = create();
+
+// eslint-disable-next-line no-undef
+const myRouter = router(path.resolve('./server/db.json'));
+
+server.use(defaults({}));
+server.use(bodyParser);
+
+// Нужно для небольшой задержки, чтобы запрос проходил не мгновенно, имитация реального апи
+
+
+
+server.use(myRouter);
+
+// запуск сервера
+server.listen(3000, () => {
+    console.log('server is running on 8000 port');
+});
