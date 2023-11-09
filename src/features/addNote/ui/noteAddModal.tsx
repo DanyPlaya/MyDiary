2b94e812d7ce6c26b85d78f7a114e55abf067943
DanyPlaya <danila.lusuy@gmail.com>
2023-11-10 00:30:33 +0300

    feat: created NoteAddModal component for add new note

--- src/features/addNote/ui/noteAddModal.tsx
+++ src/features/addNote/ui/noteAddModal.tsx
@@ -1,13 +1,114 @@
-import { Modal } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  Textarea,
+} from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { NoteAddSchema, schemaNote } from "../model/schemaForm";
+import { error } from "console";
 type NoteAddModalProps = {
   isOpen: boolean;
   onClose: () => void;
 };
 export const NoteAddModal = (props: NoteAddModalProps) => {
   const { isOpen, onClose } = props;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<NoteAddSchema>({ resolver: zodResolver(schemaNote) });
+  function onSubmit(values) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        alert(JSON.stringify(values, null, 2));
+        console.log(errors);
+        resolve();
+      }, 3000);
+    });
+  }
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit
+    <Modal
+      size={{ lg: "6xl", md: "xl", sm: "sm" }}
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      <ModalOverlay />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <ModalContent
+          borderRadius={{ lg: "40px", md: "40px", sm: "0px", base: "0px" }}
+          p={"45px 45px 53px 60px"}
+        >
+          <ModalHeader>
+            <Text
+              fontSize={{ lg: "42px", md: "42px", sm: "32px", base: "32px" }}
+              fontWeight={"700"}
+            >
+              Новая запись
+            </Text>
+            <ModalCloseButton mr={"45px"} mt={"45px"} fontSize={"25px"} />
+          </ModalHeader>
+
+          <ModalBody>
+            <Box display={{ lg: "flex" }}>
+              <FormControl
+                isInvalid={!!errors.title}
+                mb={{ md: "45px" }}
+                mr={{ lg: "42px" }}
+              >
+                <FormLabel>Заголовок</FormLabel>
+                <Input maxLength={200} type="text" {...register("title")} />
+                <FormErrorMessage>
+                  {!!errors.title && errors.title.message}
+                </FormErrorMessage>
+              </FormControl>
+              <FormControl isInvalid={!!errors.date} mb={{ md: "45px" }}>
+                <FormLabel>Дата</FormLabel>
+                <Input type="datetime-local" {...register("date")} />
+                <FormErrorMessage>
+                  {!!errors.date && errors.date.message}
+                </FormErrorMessage>
+              </FormControl>
+            </Box>
+            <FormControl isInvalid={!!errors.body}>
+              <FormLabel>Заметка</FormLabel>
+              <Textarea
+                maxLength={2000}
+                h={"166px"}
+                resize={"none"}
+                {...register("body")}
+              />
+              <FormErrorMessage>
+                {!!errors.body && errors.body.message}
+              </FormErrorMessage>
+            </FormControl>
+          </ModalBody>
+          <ModalFooter display={"flex"} justifyContent={"start"}>
+            <Button
+              h={"70px"}
+              w={"300px"}
+              color={"white"}
+              borderRadius={"65px"}
+              bg={"#050F28"}
+              type="submit"
+            >
+              Поделиться наболевшим
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </form>
     </Modal>
   );
 };
