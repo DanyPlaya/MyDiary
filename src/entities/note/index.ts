2e996864162b5b31c710aea8b03d5bbb0f8c4ac1
DanyPlaya <danila.lusuy@gmail.com>
2023-11-11 19:07:17 +0300

    feat: add public api

--- src/entities/note/index.ts
+++ src/entities/note/index.ts
@@ -1,4 +1,5 @@
 export { NoteCard } from './ui/noteCard'
 export { type Note } from './types/types'
-export { useGetNoteDetailsQuery, useGetNotesQuery, useAddNoteMutation } from './api/noteApi'
+export { useGetNoteDetailsQuery, useGetNotesQuery, useAddNoteMutation, useLazySortNotesQuery, useSortNotesQuery } from './api/noteApi'
 export { NoteCardSkeleton } from './ui/noteCardSkeleton'
+export { noteActions, noteReducer, type NoteSliceState, selectSortMode } from './model/noteSlice'
