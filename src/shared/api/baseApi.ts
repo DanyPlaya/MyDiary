80a22a42b7fc79428350b94b9aeaa5583eec2303
DanyPlaya <danila.lusuy@gmail.com>
2023-11-11 17:24:41 +0300

    feat: add tags and headers

--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -1,7 +1,13 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { NOTES_TAG } from './tags'
 export const baseApi = createApi({
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:3000'
+        baseUrl: 'http://localhost:3000',
+        prepareHeaders: (headers) => {
+            headers.set("Content-Type", "application/json")
+            return headers
+        }
     }),
+    tagTypes: [NOTES_TAG],
     endpoints: () => ({})
 })
