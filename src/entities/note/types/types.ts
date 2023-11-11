8d1b70f94c2814106f2a34f1f11e06da3dc1a1c4
DanyPlaya <danila.lusuy@gmail.com>
2023-11-11 17:26:04 +0300

    fix: rename types

--- src/entities/note/types/types.ts
+++ src/entities/note/types/types.ts
@@ -2,12 +2,17 @@ export type Note = {
     id: number,
     title: string,
     body: string,
-    dateTime: string,
+    datetime: string,
 }
 //Dto для получения всех записей
-export type ReqNotesDto = Note[]
-export type ResNotesDto = void
+export type ReqNotesDto = void
+export type ResNotesDto = Note[]
 
 //Dto для получения одной записи
-export type ReqNoteDetailsDto = Note
-export type ResNoteDetailsDto = Pick<Note, 'id'>
+export type ReqNoteDetailsDto = Pick<Note, 'id'>
+export type ResNoteDetailsDto = Note
+
+//Dto для добавления записи
+
+export type ReqAddNoteDto = Omit<Note, "id">
+export type ResAddNoteDto = void
