3da3f00e671502e05f88d61d8474861249d04ac3
DanyPlaya <danila.lusuy@gmail.com>
2023-11-11 17:22:32 +0300

    feat: create Middleware auto id

--- api/server.js
+++ api/server.js
@@ -1,29 +1,34 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import pkg from 'json-server';
 import path from 'path';
-const { create, defaults, router,rewriter } = pkg;
 
+const { create, defaults, router, rewriter } = pkg;
 
 const server = create();
-
-// eslint-disable-next-line no-undef
 const myRouter = router(path.resolve('./api/db.json'));
 
-
-
-// Нужно для небольшой задержки, чтобы запрос проходил не мгновенно, имитация реального апи
-
+// Middleware для автоматического присвоения id
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    const data = req.body || {}; // Initialize as an empty object if undefined
+    const lastNote = myRouter.db.get('notes').value().slice(-1)[0];
+    const newId = lastNote ? lastNote.id + 1 : 1;
+    data.id = newId;
+    req.body = data; // Update the req.body with the modified data
+  }
+  next();
+});
 
 server.use(defaults({}));
-
 server.use(rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-    
-}))
+  '/api/*': '/$1',
+  '/blog/:resource/:id/show': '/:resource/:id',
+}));
 server.use(myRouter);
+
 // запуск сервера
 server.listen(3000, () => {
-    console.log('server is running on 8000 port');
+  console.log('server is running on 8000 port');
 });
-export default server
+
+export default server;
