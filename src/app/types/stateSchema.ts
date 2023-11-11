6b81fa9fb566f68e1715317dc2640e38611d4aa9
DanyPlaya <danila.lusuy@gmail.com>
2023-11-11 19:06:58 +0300

    feat: add note stateSchema

--- src/app/types/stateSchema.ts
+++ src/app/types/stateSchema.ts
@@ -1,6 +1,9 @@
+
+import { NoteSliceState } from "@/entities/note"
 import { baseApi } from "@/shared/api"
 
 
 export type StateSchema = {
     [baseApi.reducerPath]: ReturnType<typeof baseApi.reducer>
+    note: NoteSliceState
 }
